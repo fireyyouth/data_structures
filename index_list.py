d5ff95fa479dede4a6451b76874c778ff07ded60
fei.han <fei.han@chaitin.com>
2019-06-16 16:01:12 +0800

    add index_list

---
+++ index_list.py
@@ -0,0 +1,165 @@
+class Node:
+    def __init__(self, value, left, right, level):
+        self.value = value
+        self._right = right
+        self._left = left
+        self.level = level
+        self.size = 1
+
+    def _update_size(self):
+        self.size = 1 + self._left.size + self._right.size
+
+    def set_left(self, left):
+        self._left = left
+        self._update_size()
+
+    def set_right(self, right):
+        self._right = right
+        self._update_size()
+
+    def left(self):
+        return self._left
+
+    def right(self):
+        return self._right
+
+Node.sentinel = Node(None, None, None, 0)
+Node.sentinel.size = 0
+
+class IndexList:
+    def __init__(self):
+        self._root = Node.sentinel
+
+    def _right_rotate(self, node):
+        t = node.left()
+        node.set_left(t.right())
+        t.set_right(node)
+        return t
+
+    def _left_rotate(self, node):
+        t = node.right()
+        node.set_right(t.left())
+        t.set_left(node)
+        return t
+    
+    def _skew(self, node):
+        assert type(node) == Node and node.level > 0
+        if node.left().level == node.level:
+            node = self._right_rotate(node)
+        return node
+    
+    def _split(self, node):
+        assert type(node) == Node and node.level > 0
+        if node.right().right() != None and node.level == node.right().level == node.right().right().level:
+            node = self._left_rotate(node)     
+            node.level += 1
+        return node
+   
+    def _insert(self, node, i, value):
+        if not (0 <= i <= node.size):
+            raise IndexError('index out of bound')
+
+        if node.level == 0:
+            return Node(value, Node.sentinel, Node.sentinel, 1)
+
+        k = 0
+        if node.left() != None:
+            k = node.left().size
+        
+        if i <= k:
+            node.set_left(self._insert(node.left(), i, value))
+        else:
+            node.set_right(self._insert(node.right(), i - (k + 1), value))
+
+        node = self._skew(node)
+        node = self._split(node)
+        
+        return node
+    
+    def _succecor(self, node):
+        node = node.right()
+        while node.left().level > 0:
+            node = node.left()
+        return node
+    
+    def _swap(self, a, b):
+        a.value, b.value = b.value, a.value
+
+    def _remove(self, node, i):
+        if not (0 <= i < node.size):
+            raise IndexError('index out of bound')
+        
+        kid = -1
+        
+        k = 0
+        if node.left() != None:
+            k = node.left().size
+
+        if i == k:
+            if node.right().level == 0:
+                return Node.sentinel
+            else:
+                t = self._succecor(node)
+                self._swap(t, node)
+                kid = 1
+         
+        if kid == -1:
+            kid = 0 if i < k else 1
+
+        if kid == 0:
+            node.set_left(self._remove(node.left(), i))
+            if node.left().level < node.level - 1:
+                assert node.left().level == node.level - 2
+                if node.right().level < node.level:
+                    node.level -= 1
+                    node = self._split(node)
+                else:
+                    node.level -= 1
+                    node.right().level -= 1
+                    node.set_right(self._skew(node.right()))
+                    node.right().set_right(self._skew(node.right().right()))
+                    node = self._split(node)
+                    node.set_right(self._split(node.right()))
+        else:
+            node.set_right(self._remove(node.right(), i - k - 1))
+            if node.right().level < node.level - 1:
+                assert node.right().level == node.level - 2
+                node.level -= 1
+                node = self._skew(node)
+                node.set_right(self._skew(node.right()))
+                node = self._split(node)
+        
+        return node 
+
+    def insert(self, i, value):
+        self._root = self._insert(self._root, i, value)
+    
+    def remove(self, i):
+        self._root = self._remove(self._root, i)
+    
+    def _show(self, node, s):
+        if node.level > 0:
+            self._show(node.right(), s + 1)
+            print(s * '\t', (node.value, node.level))
+            self._show(node.left(), s + 1)
+
+    def show(self):
+        self._show(self._root, 0)
+    
+    def get(self, i):
+        node = self._root
+        while node != Node.sentinel:
+            k = 0
+            if node.left() != None:
+                k = node.left().size
+            if i == k:
+                return node.value
+            elif i < k:
+                node = node.left()
+            else:
+                node = node.right()
+                i -= k + 1
+        raise IndexError('index out of bound')
+    
+    def __len__(self):
+        return self._root.size
---
+++ index_list_test.py
@@ -0,0 +1,47 @@
+from index_list import IndexList
+import unittest
+
+class IndexListTest(unittest.TestCase):
+    def test_normal(self):
+        a = IndexList()
+        b = []
+        for i in range(1000):
+            a.insert(len(a), i)
+            b.append(i)
+        
+        self.assertEqual(len(a), len(b))
+        for i in range(len(a)):
+            self.assertEqual(a.get(i), b[i])
+
+        a.insert(2, -1)
+        b.insert(2, -1)
+
+        self.assertEqual(len(a), len(b))
+        for i in range(len(a)):
+            self.assertEqual(a.get(i), b[i])
+
+        a.remove(5)
+        b.pop(5)
+
+        self.assertEqual(len(a), len(b))
+        for i in range(len(a)):
+            self.assertEqual(a.get(i), b[i])
+
+    def test_exception(self):
+        a = IndexList()
+
+        with self.assertRaises(IndexError):
+            a.get(0)
+
+        with self.assertRaises(IndexError):
+            a.insert(1, 0)
+
+        a.insert(0, 0)
+        self.assertEqual(a.get(0), 0)
+
+        with self.assertRaises(IndexError):
+            a.remove(1)
+
+        a.remove(0)
+
+unittest.main()
